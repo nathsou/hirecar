99bfb29f4c45b3fe272e61b6bde089f0cc676aa1
nathalie <mlle.zhang.nathalie@gmail.com>
2019-04-15 17:48:05 +0200

    Réorganisation des fichiers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './res/img/logo.svg';
+import './res/style/App.css';
 
 class App extends Component {
   render() {
