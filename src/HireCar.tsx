5e01c52fd28be70d9a9f8ba751ba6e8c26545ae1
nathsou <nathsou@users.noreply.github.com>
2019-04-16 14:14:48 +0200

    Added basic routing

--- src/HireCar.tsx
+++ src/HireCar.tsx
@@ -3,16 +3,18 @@ import HcJumbotron from './components/HcJumbotron';
 import HcNavbar from './components/HcNavbar';
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HcParkingSearch from './components/HcParkingSearch';
 
 export default class HireCar extends Component {
   public render() {
     return (
-
       <Provider store={configureStore()}>
-        {/* <HcPrimaryButton>LOCATION DE PARKING</HcPrimaryButton>
-        <HcPrimaryButton outlined='true'>LOCATION DE VOITURE</HcPrimaryButton>*/}
-        <HcNavbar />
-        <HcJumbotron />
+        <BrowserRouter>
+          <HcNavbar />
+          <Route exact path='/' component={HcJumbotron} />
+          <Route exact path='/parking' component={HcParkingSearch} />
+        </BrowserRouter>
       </Provider>
     );
   }
