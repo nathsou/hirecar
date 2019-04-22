e61e82f22d04f3e741acd3d6b35ed9be7409167e
nathalie <mlle.zhang.nathalie@gmail.com>
2019-04-22 19:14:52 +0200

    Responsive : rent parking

--- src/components/HcParkingSearch.tsx
+++ src/components/HcParkingSearch.tsx
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import ReactMapGL from 'react-map-gl';
 import HcList from "./HcList";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default class HcParkingSearch extends Component {
+
+    private nabvarHeigth: number = 52;
+    private footerHeigth: number = 82;
+
 
     public state = {
         viewport: {
             width: '60%',
-            height: '100vh',
+            height: `calc(100vh - ${this.nabvarHeigth + this.footerHeigth}px)`,
             latitude: 47.3,
             longitude: 2.2,
             zoom: 4.8
@@ -16,15 +23,24 @@ export default class HcParkingSearch extends Component {
 
     public render() {
         return (
-            <main className='hc-rent-container'>
-                <HcList />
-
-                <ReactMapGL
-                    {...this.state.viewport}
-                    onViewportChange={(viewport) => this.setState({ viewport })}
-                    mapboxApiAccessToken='pk.eyJ1IjoibmF0aHNvdTMiLCJhIjoiY2p1cHVpN24wMWxrZTQ0bzhrdDN3MHd2aCJ9.X2KA-7edhC_vD3veUEssgA'
-                    mapStyle='mapbox://styles/mapbox/streets-v11'
-                />
+            <main>
+                <Container>
+                    <Row>
+                        <Col lg={5}>
+                            <HcList />
+                        </Col>
+                        <Col lg={7}>
+                            <div className="hc-maps">
+                                <ReactMapGL
+                                    {...this.state.viewport}
+                                    onViewportChange={(viewport) => this.setState({ viewport })}
+                                    mapboxApiAccessToken='pk.eyJ1IjoibmF0aHNvdTMiLCJhIjoiY2p1cHVpN24wMWxrZTQ0bzhrdDN3MHd2aCJ9.X2KA-7edhC_vD3veUEssgA'
+                                    mapStyle='mapbox://styles/mapbox/streets-v11'
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </main>
         );
     }
