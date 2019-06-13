115ac94fe32a89fb5950c792fe85a6dd265d5897
guillaumegilles <guillaume.gilles@utt.fr>
2019-06-13 16:27:07 +0200

    modification pour correspondre au paiement dans paypal

--- src/components/HcPaypal.tsx
+++ src/components/HcPaypal.tsx
@@ -2,29 +2,32 @@ import React,{Component} from "react";
 // @ts-ignore
 import PaypalExpressBtn from 'react-paypal-express-checkout';
 interface HcPaypalProps {
-    prix:number
+    prix:number,
+    email:string,
+    name:string
 }
 
 export default class HcPaypal extends Component<HcPaypalProps>{
 
     public render(){
         const {prix} =this.props;
+        const successPaiment = this.successPaiment;
         const client ={
             sandbox:process.env.REACT_APP_PAYPAl_CLIENT_ID
     };
         return ( <PaypalExpressBtn client={client} currency={'EUR'} total={prix} onSuccess={successPaiment} />)
     }
+    // @ts-ignore
+    public successPaiment = (paiement) => {
+        console.log(JSON.stringify(paiement));
+        const site = process.env.REACT_APP_HIRECARAPI+`/verify_paiement?mail=${this.props.email}&name=${this.props.name}&price=${this.props.prix}`;
+        const req= new XMLHttpRequest();
+        req.open('POST',site,false);
+        req.send(JSON.stringify(paiement));
+        if (req.status === 200){
+            //TODO complete in case of succeed Action
+        } else {
+            //TODO complete in case of unpaid error given by data trasnmitted to the serveur
+        }
+
 }
-// @ts-ignore
-const successPaiment = (paiement) => {
-    console.log(JSON.stringify(paiement));
-    const site = process.env.REACT_APP_HIRECARAPI+'/verify_paiement';
-    const req= new XMLHttpRequest();
-    req.open('POST',site,false);
-    req.send(JSON.stringify(paiement));
-    if (req.status === 200){
-        //TODO complete in case of succeed Action
-    } else {
-        //TODO complete in case of unpaid error given by data trasnmitted to the serveur
-    }
-};
